401a79ff5eeb12afa793e11a27596773639e7104
Ed <jr-lol@live.com>
2020-04-10 20:53:13 -0300

    adicionado contexto e realizado primeiro migration

---
+++ RepositoryPattern/Infrastructures/DBConfiguration/DatabaseConnection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Infrastructures.DBConfiguration
+{
+    class DatabaseConnection
+    {
+        public static IConfiguration Connectionconfiguration
+        {
+            get
+            {
+                IConfigurationRoot Configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                return Configuration;
+
+            }
+        }
+    }
+}
---
+++ RepositoryPattern/Infrastructures/DBConfiguration/EFCore/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructures.DBConfiguration.EFCore
+{
+    class ApplicationContext: DbContext
+    {
+        public ApplicationContext() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            if (!dbContextOptionsBuilder.IsConfigured)
+            {
+                dbContextOptionsBuilder.UseSqlServer(DatabaseConnection.Connectionconfiguration
+                                                     .GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public DbSet<User> User { get; set; }
+
+        public DbSet<TaskToDo> TaskToDo { get; set; }
+
+    }
+}
